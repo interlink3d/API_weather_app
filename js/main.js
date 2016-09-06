f34bcba3c1a5dc0b81452abc20b650005ad0449f
interlink3d <interlink3d@gmail.com>
2016-09-06 10:14:27 -0500

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 "use strict";
 
-// let $ = require('jquery'),
-//     db = require("./db-interaction"),
-//     templates = require("./dom-builder"),
-//     login = require("./user");
+let $ = require('jquery'),
+    db = require("./db-interaction"),
+    login = require("./user");
 
 var userId = "";
 
@@ -13,8 +12,6 @@ $("#zipButton").click(function () {
   var zipCode = $("#inputZip").val();
   console.log(zipCode)
 
-    // function getCurrent(callback) {
-    // return new Promise(function (resolve, reject) {
       $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather?zip="+zipCode+",us&units=imperial&appid=5d7f032279d8bc6257354baea46f3877"
       }).done(function (currentWeather) {
@@ -23,9 +20,7 @@ $("#zipButton").click(function () {
         loadWeather(currentWeather);
       });
     });
-  // }
 
-// });
 
 var loadWeather = function (currentWeather) {
   console.log(currentWeather);
@@ -44,89 +39,6 @@ var loadWeather = function (currentWeather) {
 };
 
 
-
-
-
-
-
-Using the REST API
-// function currentWeather() {
-//   $(".uiContainer--wrapper").html("");  // cleared the DOM before loading songs
-//   db.getSongs()
-//     .then(function(songData) {
-//       var idArr = Object.keys(songData);   // Object.keys takes the keys for that object and puts them into an array
-//       idArr.forEach(function(key) {
-//         songData[key].id = key;           // using [] because it is a variable that we are passing in
-//       });
-//       console.log("songObject with ID added", songData);
-//       templates.makeSongList(songData);
-//     });
-// }
-
-// function threeDay() {
-//   $(".uiContainer--wrapper").html("");  // cleared the DOM before loading songs
-//   db.getSongs()
-//     .then(function(songData) {
-//       var idArr = Object.keys(songData);   // Object.keys takes the keys for that object and puts them into an array
-//       idArr.forEach(function(key) {
-//         songData[key].id = key;           // using [] because it is a variable that we are passing in
-//       });
-//       console.log("songObject with ID added", songData);
-//       templates.makeSongList(songData);
-//     });
-// }
-
-// function sevenDay() {
-//   $(".uiContainer--wrapper").html("");  // cleared the DOM before loading songs
-//   db.getSongs()
-//     .then(function(songData) {
-//       var idArr = Object.keys(songData);   // Object.keys takes the keys for that object and puts them into an array
-//       idArr.forEach(function(key) {
-//         songData[key].id = key;           // using [] because it is a variable that we are passing in
-//       });
-//       console.log("songObject with ID added", songData);
-//       templates.makeSongList(songData);
-//     });
-// }
-
-
-// // Send saved forecast data to db then reload DOM with current forecast
-// $(document).on("click", ".save_new_btn", function() {
-//   let songObj = buildSongObj();
-//   db.addSong(songObj)
-//   .then(function (songId) {
-//     console.log("song saved", songId);
-//     loadSongsToDOM();
-//   });
-// });
-
-
-// //Save edited song to FB then reload DOM with updated song data
-// $(document).on("click", ".save_edit_btn", function() {
-//   let songObj = buildSongObj(),
-//       songId = $(this).attr("id");
-//   db.editSong(songObj, songId)
-//   .then(function (data) {
-//     loadSongsToDOM();
-//   });
-// });
-
-// // Remove song then reload the DOM w/out new song
-// $(document).on("click", ".delete-btn", function () {
-//   let songId = $(this).data("delete-id");
-//   db.deleteSong(songId)
-//   .then(function (data) {
-//     loadSongsToDOM();
-//   });
-// });
-
-// // View songs from the list created in FB
-// $(document).on("click", "#view-songs", function () {
-//   loadSongsToDOM();
-// });
-
-
-
 //***************************************************************
 // User login section. Should ideally be in its own module
 $("#auth-btn").click(function() {
@@ -141,25 +53,3 @@ $("#auth-btn").click(function() {
   });
 });
 //****************************************************************
-
-// Helper functions for forms stuff. Nothing related to Firebase
-// Build a song obj from form data.
-function buildSongObj() {
-    let songObj = {
-    title: $("#form--title").val(),
-    artist: $("#form--artist").val(),
-    album: $("#form--album").val(),
-    year: $("#form--year").val(),
-    uid: userId
-  };
-  return songObj;
-}
-
-// Load the new song form
-$("#add-song").click(function() {
-  console.log("clicked add song");
-  var songForm = templates.songForm()
-  .then(function(songForm) {
-    $(".uiContainer--wrapper").html(songForm);
-  });
-});
